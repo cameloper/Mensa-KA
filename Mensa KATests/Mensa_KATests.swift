ce31d4c28484a7a29f474540c8232d1ad6a0a049
Batughan Yilmaz <ihsan.yilmaz@exxeta.com>
2023-06-20 21:42:40 +0200

    Two more changes

--- Mensa KATests/Mensa_KATests.swift
+++ Mensa KATests/Mensa_KATests.swift
@@ -8,7 +8,7 @@
 import XCTest
 @testable import Mensa_KA
 
-final class KA_MensaTests: XCTestCase {
+final class Mensa_KATests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
